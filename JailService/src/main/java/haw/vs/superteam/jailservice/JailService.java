0c2ffb738184628c2c8324a368c94a2bbfdadd0a
flbaue <florian.bauer@posteo.de>
2015-11-28 15:35:49 +0100

    added empty jail service

---
+++ JailService/src/main/java/haw/vs/superteam/jailservice/JailService.java
@@ -0,0 +1,15 @@
+package haw.vs.superteam.jailservice;
+
+/**
+ * Created by florian on 28.11.15.
+ */
+public class JailService {
+
+    public static void main(String[] args) {
+        new JailService().run();
+    }
+
+    private void run() {
+        //TODO
+    }
+}
