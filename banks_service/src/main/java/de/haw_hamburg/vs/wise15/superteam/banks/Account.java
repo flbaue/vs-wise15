445fe5fe64ae0202467d3d58a63eade3a629581c
Maria Korennyh <maria.korennyh@haw-hamburg.de>
2015-12-13 13:43:56 +0100

    banks fertig

--- banks_service/src/main/java/de/haw_hamburg/vs/wise15/superteam/banks/Account.java
+++ banks_service/src/main/java/de/haw_hamburg/vs/wise15/superteam/banks/Account.java
@@ -1,19 +1,28 @@
 package de.haw_hamburg.vs.wise15.superteam.banks;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+
 import java.math.BigDecimal;
 
 /**
  * Created by masha on 16.11.15.
  */
 public class Account {
+    Gson gson;
 
     private Player player;
     private BigDecimal saldo;
 
 
-    public Account(Player player) {
-        this.player = player;
-        this.saldo = new BigDecimal(200);
+    public Account(String player, String saldo) {
+        try {
+            this.player = gson.fromJson(player, Player.class);
+        }catch (JsonSyntaxException e){
+            System.out.println("account create: JsonSyntaxException");
+        }
+
+        this.saldo = new BigDecimal(saldo);
     }
 
     public Player getPlayer() {
