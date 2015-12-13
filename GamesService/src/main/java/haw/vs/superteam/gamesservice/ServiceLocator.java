7b8727cfc98e7e3e0ddc8195aadb726563c4a243
flbaue <florian.bauer@posteo.de>
2015-12-13 14:55:13 +0100

    games service: refactoring

--- GamesService/src/main/java/haw/vs/superteam/gamesservice/ServiceLocator.java
+++ GamesService/src/main/java/haw/vs/superteam/gamesservice/ServiceLocator.java
@@ -1,11 +1,8 @@
 package haw.vs.superteam.gamesservice;
 
-import com.google.gson.Gson;
 import haw.vs.superteam.gamesservice.api.ServicesAPI;
 import haw.vs.superteam.gamesservice.model.Service;
-import retrofit.GsonConverterFactory;
 import retrofit.Response;
-import retrofit.Retrofit;
 
 import java.io.IOException;
 
@@ -16,15 +13,9 @@ public class ServiceLocator {
 
     private ServicesAPI servicesAPI;
 
-    public ServiceLocator() {
+    public ServiceLocator(ServicesAPI servicesAPI) {
 
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(Constants.SERVICE_DIRECTORY_URL + "/")
-                .addConverterFactory(GsonConverterFactory.create(new Gson()))
-                .client(Utils.getUnsafeOkHttpClient())
-                .build();
-
-        servicesAPI = retrofit.create(ServicesAPI.class);
+        this.servicesAPI = servicesAPI;
     }
 
     public Service getBoardsService() throws IOException {
