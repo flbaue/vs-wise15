c5f0d8c3d2e458d9fbb4dd55f94c2d8b908caae1
Maria Korennyh <maria.korennyh@haw-hamburg.de>
2015-12-13 15:45:02 +0100

    events registred

--- events_service/src/main/java/de/haw_hamburg/vs/wise15/superteam/events/EventService.java
+++ events_service/src/main/java/de/haw_hamburg/vs/wise15/superteam/events/EventService.java
@@ -1,10 +1,26 @@
 package de.haw_hamburg.vs.wise15.superteam.events;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonSyntaxException;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLContexts;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import spark.Request;
 import spark.Response;
 
+import javax.net.ssl.SSLContext;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -17,6 +33,7 @@ import static spark.Spark.*;
 public class EventService {
 
     Gson gson = new Gson();
+    private String ip;
     Map<String,ArrayList<Event>> events = new HashMap<>();
     Integer id;
     Integer subsId;
@@ -28,6 +45,9 @@ public class EventService {
     }
 
     private void run() {
+        //anmeldung
+        //https://vs-docker.informatik.haw-hamburg.de/ports/8053/services
+        register();
         System.out.println("PlayerService is starting");
 
         //List of available event
@@ -50,6 +70,46 @@ public class EventService {
 
         //post (uri of the subscroption)
     }
+
+    private void register() {
+        try {
+            ip = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        JsonObject json = new JsonObject();
+        json.addProperty("name", "SuperTeamEventsService");
+        json.addProperty("description", "Event Service von SuperTeam");
+        json.addProperty("service","events");
+        json.addProperty("uri","https://vs-docker.informatik.haw-hamburg.de/cnt/"+ ip+"/4567");
+        try {
+            SSLContext sslcontext = null;
+            try {
+                sslcontext = SSLContexts.custom().loadTrustMaterial(null, new TrustSelfSignedStrategy()).build();
+            } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+            } catch (KeyManagementException e) {
+                e.printStackTrace();
+            } catch (KeyStoreException e) {
+                e.printStackTrace();
+            }
+
+            SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslcontext);
+            CloseableHttpClient httpclient = HttpClients.custom()
+                    .setSSLSocketFactory(sslsf)
+                    .build();
+            Unirest.setHttpClient(httpclient);
+            //sout
+            System.out.println(json.toString());
+            HttpResponse<JsonNode> jsonNodeHttpResponse = Unirest.post("https://vs-docker.informatik.haw-hamburg.de/ports/8053/services")
+                    .header("Content-Type", "application/json")
+                    .body(json.toString()).asJson();
+            System.out.println(jsonNodeHttpResponse.getStatus());
+        } catch (UnirestException e) {
+            e.printStackTrace();
+        }
+    }
+
 
     private Object postEventSubscriptions(Request request, Response response) {
         subsId++;
